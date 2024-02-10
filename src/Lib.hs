c5d10dc2b36b6d51976c7327e062d1c4c1d42881
cchenkov <chudomir.chenkov31@gmail.com>
2024-02-10 15:45:59 +0200

    parser - initial non-monadic implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,6 +20,42 @@ data Expr = Var Ident
           | Apply Expr [Expr]
           deriving (Show)
 
+type Parser a = String -> [(a, String)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p f = \inp -> concat [f v rest | (v, rest) <- p inp]
+
+result :: a -> Parser a
+result v = \inp -> [(v, inp)]
+
+zero :: Parser a
+zero = \_ -> []
+
+item :: Parser Char
+item = 
+  \inp -> case inp of
+    [] -> []
+    (x:xs) -> [(x, xs)]
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = 
+  item `bind` \x ->
+    if p x
+      then result x
+      else zero
+
+char :: Char -> Parser Char
+char x = sat (\y -> x == y)
+
+digit :: Parser Char
+digit = sat (\x -> '0' <= x && x <= '9')
+
+lower :: Parser Char
+lower = sat (\x -> 'a' <= x && x <= 'z')
+
+upper :: Parser Char
+upper = sat (\x -> 'A' <= x && x <= 'Z')
+
 lookup :: String -> Context -> Maybe Value 
 lookup _ [] = Nothing
 lookup x ((k, v) : r) 
